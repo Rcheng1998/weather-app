a5b42a1cad073954901a2648e01e4c69bd8a4d4a
Rickie Cheng <36807517+Rcheng1998@users.noreply.github.com>
2020-01-01 01:12:20 -0500

    Update footer.component.jsx
    fixed linked to github

--- src/app_component/footer.component.jsx
+++ src/app_component/footer.component.jsx
@@ -7,7 +7,7 @@ import './footer.style.css';
 const Footer = ()=>{
     return(
         <div className="container pt-3">
-            <a href={"https://rickiecheng.com"}> <img src={githublogo} alt="githublogo"/></a>
+            <a href={"https://github.com/Rcheng1998/weather-app"}> <img src={githublogo} alt="githublogo"/></a>
             <a href={"https://rickiecheng.com"}> <img src={userlogo} alt="userlogo"/></a>
 
             <div className="container">
